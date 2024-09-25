29fa5283c8c56527866e6e160e7570746e9986db
Marcos Azevedo <m4rcos.azevedo@gmail.com>
2024-09-25 14:35:32 -0300

    feat: added collection view in list

--- src/pages/collection.tsx
+++ src/pages/collection.tsx
@@ -1,9 +1,25 @@
 import { ProductItem } from '@/components/product-item'
+import { ProductTableItem } from '@/components/product-table-item'
 import { Loading, TextField, Title } from '@/components/ui'
 import { useProducts, useProductsFiltered } from '@/hooks'
-import { Autocomplete, Box, Button, Grid, Link } from '@mui/material'
+import {
+  Autocomplete,
+  Box,
+  Button,
+  Grid,
+  Link,
+  MenuItem,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow
+} from '@mui/material'
+import { useState } from 'react'
 
 const Collection = () => {
+  const [view, setView] = useState('grid')
   const { products, loading, platforms, statuses, types } = useProducts()
   const { filteredProducts, handleFilter } = useProductsFiltered(products)
 
@@ -16,11 +32,18 @@ const Collection = () => {
       <Title>Coleção de Jogos E Actions Figures</Title>
 
       <Box gap={4} flexDirection="column" flexGrow={1} display="flex">
-        <Link href="/collection/item/create" underline="hover">
-          <Button variant="contained" color="primary" size="small">
-            Cadastrar item
-          </Button>
-        </Link>
+        <section className="flex justify-between">
+          <Link href="/collection/item/create" underline="hover">
+            <Button variant="contained" color="primary">
+              Cadastrar item
+            </Button>
+          </Link>
+
+          <Select value={view} size="small" onChange={event => setView(event.target.value)}>
+            <MenuItem value="grid">Visualizar Grid</MenuItem>
+            <MenuItem value="list">Visualizar Lista</MenuItem>
+          </Select>
+        </section>
 
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={4} lg={3}>
@@ -60,13 +83,32 @@ const Collection = () => {
 
         <p>{filteredProducts.length} Unidades</p>
 
-        <Grid container spacing={3}>
-          {filteredProducts.map(product => (
-            <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-              <ProductItem product={product} />
-            </Grid>
-          ))}
-        </Grid>
+        {view === 'grid' ? (
+          <Grid container spacing={3}>
+            {filteredProducts.map(product => (
+              <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                <ProductItem product={product} />
+              </Grid>
+            ))}
+          </Grid>
+        ) : (
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Imagem</TableCell>
+                <TableCell>Nome</TableCell>
+                <TableCell>Tipo</TableCell>
+                <TableCell>Plataforma</TableCell>
+                <TableCell>Classificação</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredProducts.map(product => (
+                <ProductTableItem product={product} key={product.id} />
+              ))}
+            </TableBody>
+          </Table>
+        )}
       </Box>
     </Box>
   )
