f66c09b4ac373d55300e315a4edd67675b47f8f7
Marcos Azevedo <m4rcos.azevedo@gmail.com>
2024-09-30 10:39:29 -0300

    chore: add github actions

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,6 @@
 export * from './use-auth-check'
 export * from './use-collection-item-form'
 export * from './use-platforms'
-export * from './use-populate-with-asin'
 export * from './use-product'
 export * from './use-products'
 export * from './use-products-filtered'
